d397e3718a0b8de1e25b2271b47445f23724f9b5
Aviral00Jain <jainaviral183@gmail.com>
2026-03-07 15:53:56 +0530

    [aviral]: Added a program of service

---
+++ src/main/java/com/addressbookapp/service/AddressBook.java
@@ -0,0 +1,19 @@
+package com.addressbookapp.service;
+
+//This is HAS-A relationship (Aggregation).
+import com.addressbookapp.model.Contact;
+
+import java.util.ArrayList;
+
+public class AddressBook {
+    ArrayList<Contact> contacts = new ArrayList<>();
+    public void addContact(Contact contact){
+        contacts.add(contact);
+    }
+
+    public void displayContacts(){
+        for(Contact c : contacts){
+            c.display();
+        }
+    }
+}
