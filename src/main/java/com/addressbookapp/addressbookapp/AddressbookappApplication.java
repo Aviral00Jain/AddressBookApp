bc54570ec7deb2882a5459213068b501045f8694
Aviral00Jain <jainaviral183@gmail.com>
2026-03-07 15:07:17 +0530

    [Aviral]: added a program of addressbookapplication

--- src/main/java/com/addressbookapp/addressbookapp/AddressbookappApplication.java
+++ src/main/java/com/addressbookapp/addressbookapp/AddressbookappApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class AddressbookappApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to Address Book Program");
 		SpringApplication.run(AddressbookappApplication.class, args);
 	}
 
