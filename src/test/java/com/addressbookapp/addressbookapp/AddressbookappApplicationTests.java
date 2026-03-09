c32218456182bf6409adef66edb9c5d392ddf128
Aviral00Jain <jainaviral183@gmail.com>
2026-03-09 14:46:29 +0530

    [Aviral]: modified a program of test

--- src/test/java/com/addressbookapp/addressbookapp/AddressbookappApplicationTests.java
+++ src/test/java/com/addressbookapp/addressbookapp/AddressbookappApplicationTests.java
@@ -1,13 +1,25 @@
 package com.addressbookapp.addressbookapp;
 
+import com.addressbookapp.service.AddressBook;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 @SpringBootTest
-class AddressbookappApplicationTests {
+class AddressBookAppApplicationTests {
 
 	@Test
 	void contextLoads() {
+	}
+
+	@Test
+	void testLoadContactsFromDB() {
+		AddressBook ab = new AddressBook();
+		ab.loadContactsFromDB();
+		assertNotNull(ab.getContacts());
+		assertTrue(ab.getContacts().size() >= 0);
 	}
 
 }
