8854d72d5004142cd2bfb1a92d155f4e993c40cb
Aviral00Jain <jainaviral183@gmail.com>
2026-03-09 14:51:58 +0530

    [Aviral]: modified a program of address book main

--- src/main/java/com/addressbookapp/addressbookapp/AddressBookMain.java
+++ src/main/java/com/addressbookapp/addressbookapp/AddressBookMain.java
@@ -7,7 +7,7 @@ import java.util.*;
 
 public class AddressBookMain {
 
-    public static void main(String[] args) {
+    public static    void main(String[] args) {
 
         Scanner sc = new Scanner(System.in);
         Map<String, AddressBook> addressBooks = new HashMap<>();
